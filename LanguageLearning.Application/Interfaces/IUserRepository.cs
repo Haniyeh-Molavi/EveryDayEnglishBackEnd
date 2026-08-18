40d8b98fb65ffe88b2b135dea0496fe25f07a55f
Haniyeh-Molavi <haniyeh.molavi.98@gmail.com>
2026-08-18 10:30:50 +0100

    Rmove project

--- LanguageLearning.Application/Interfaces/IUserRepository.cs
+++ LanguageLearning.Application/Interfaces/IUserRepository.cs
@@ -1,8 +1,10 @@
-﻿using LanguageLearning.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LanguageLearning.Domain.Entities;
 
 public interface IUserRepository
 {
     Task<User?> GetByEmailAsync(string email);
-
     Task AddAsync(User user);
 }
--- LanguageLearning.Infrastructure/Program.cs
+++ LanguageLearning.Infrastructure/Program.cs
@@ -1,6 +1,10 @@
 ﻿using LanguageLearning.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddInfrastructure();
